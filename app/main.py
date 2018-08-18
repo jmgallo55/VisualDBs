c6959447d6d99d1091ca7758970bf2faa9b2f8a3
Joe Gallo <jmgallo55@gmail.com>
2018-08-18 14:07:33 -0700

    restart with GitHub import

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from config import Config
+from app.forms import LoginForm
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from flask import render_template
 
@@ -20,5 +23,13 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html',  title='Sign In', form=form)
 #if __name__ == "__main__":
  #   app.run(host='0.0.0.0', debug=True, port=80)
