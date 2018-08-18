2877a1741bb89cbed204e1378e1933ff0039c38c
Joe Gallo <jmgallo55@gmail.com>
2018-08-18 13:37:01 -0700

    restart with GitHub import

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
-from flask import Flask
+from flask import render_template
+from app import app
 
-app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
 #if __name__ == "__main__":
  #   app.run(host='0.0.0.0', debug=True, port=80)
