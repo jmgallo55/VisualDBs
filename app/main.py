5bddd73c68ff92ce5e6a1569750c37d732ce2731
Joe Gallo <jmgallo55@gmail.com>
2018-08-18 13:44:45 -0700

    restart with GitHub import

--- app/main.py
+++ app/main.py
@@ -2,9 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-#from flask import render_template
-from app import app
-
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
