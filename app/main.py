82fa25fbe1f8e55a9709ec475e78b45a6ec4be58
Joe Gallo <jmgallo55@gmail.com>
2018-08-18 12:50:11 -0700

    restart with GitHub import

--- app/main.py
+++ app/main.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World from Flask in a uWSGI Nginx Docker container with \
-     Python 3.6 (default)"
+    return "Finally, it is working"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=80)
+#if __name__ == "__main__":
+ #   app.run(host='0.0.0.0', debug=True, port=80)
